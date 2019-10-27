652e002b18fbf024ad6c2023a92fcf02e34d2b45
misato0104 <tosamimayamika@gmail.com>
2019-10-28 07:02:02 +0900

    add resources-groups (routes.rb)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
