7bbd6fc7c258cd637fb7d1a51246076d9bce0ee7
misato0104 <tosamimayamika@gmail.com>
2019-10-30 19:06:25 +0900

    new_create message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,8 @@
-#メッセージ送信用のformタグに.js-formという名のクラスが適用されている前提
-$('.js-form').on('submit', function(){
-  console.log('hoge');
-});
+$(function(){
+}
+
+
+// #メッセージ送信用のformタグに.js-formという名のクラスが適用されている前提
+// $('.js-form').on('submit', function(){
+//   console.log('hoge');
+// });
