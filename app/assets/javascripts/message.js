5210b90a3f99a110dc4972886e6287ece600a73f
misato0104 <tosamimayamika@gmail.com>
2019-10-30 21:51:42 +0900

    rewrite image有無

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -16,7 +16,7 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
+         ${image}
        </div>`
      return html;
    } else {
