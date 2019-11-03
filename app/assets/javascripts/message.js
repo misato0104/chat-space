2e878b87126942ef3b2c82f269c530352e9bd047
misato0104 <tosamimayamika@gmail.com>
2019-11-03 15:39:50 +0900

    rewrite message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,52 @@
-$(function() {
-  function buildHTML(message) {
+$(function(){ 
+  function buildHTML(message){
+
+    image = (message.image)? `<img class="lower-message__image" src="${message.image}">` : "";
+
     var html =  `
-                  <div class="message" data-message-id='${message.id}'>
+                  <div class="message" data-message-id=${message.id}>
                     <div class="upper-message">
                       <div class="upper-message__user-name">
                         ${message.user_name}
                       </div>
                       <div class="upper-message__date">
-                        ${message.data}
+                        ${message.date}
                       </div>
                     </div>
                     <div class="lower-message">
                       <p class="lower-message__content">
                         ${message.content}
                       </p>
-                      ${image}
                     </div>
+                    ${image}
                   </div>
                 `
     return html;
   }
+
+  $('.js-form').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false;
+  });
 
   var reloadMessages = function() {
     last_message_id = $('.message:last').data('messade-id');
@@ -34,14 +61,8 @@ $(function() {
       messages.forEach(function (message) {
         insertHTML = buildHTML(message);
         $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-
-      //メッセージが入ったHTMLを取得
-
-      //メッセージを追加
-
     })
     .fail(function() {
       alert('自動更新に失敗しました');
